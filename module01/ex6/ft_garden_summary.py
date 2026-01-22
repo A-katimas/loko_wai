240a87a36dd2cb9a785f3415aaaae2d66ee46ad9
Julie Tardieu <jtardieu@k1r1p4.42mulhouse.fr>
2026-01-22 14:29:54 +0100

    bah ca passe j'espert juste que ca va pas bouger

--- module01/ex6/ft_garden_summary.py
+++ module01/ex6/ft_garden_summary.py
@@ -6,7 +6,15 @@
 #    By: jtardieu <jtardieu@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/01/21 17:42:05 by jtardieu          #+#    #+#              #
-#    Updated: 2026/01/21 17:42:06 by jtardieu         ###   ########.fr        #
+#    Updated: 2026/01/22 14:28:55 by jtardieu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+def ft_garden_summary():
+    garden = str(input("Enter garden name:"))
+    plante = int(input("Enter number of plants:"))
+    print("garden : "+garden)
+    print(f"plante : {plante}")
+    print("Status: Growing well!")
+    
+ft_garden_summary()
