bcdc8bf5674e6aad82930f438118fac22cef6258
Julie Tardieu <jtardieu@k1r2p6.42mulhouse.fr>
2026-02-25 17:37:04 +0100

    il est vivaaaaaaannnnntttt ca marche c'est pas fini mais fuck

--- module03/ex5/ft_data_stream.py
+++ module03/ex5/ft_data_stream.py
@@ -4,39 +4,31 @@ import random
 class Game_Even_Generator:
     def __init__(self, player: list[dict], action: dict = {}):
         self.player = player
-        self.action = list(action)
+        self.event = action
 
     def generate_event(self, nb):
+        event_tab= list(self.event)
         for i in range(1, nb + 1):
             player = random.choice(self.player)
             level = random.randint(1, 20)
-            action = random.choice(self.action)
+            player["lvl"] += level
+            action = random.choice(event_tab)
+            self.event[action] += 1
             yield f"Event {i}: Player {player["name"]} (level {level}) {action}"
 
 
-class EventAnalyzer:
-    def __init__(slef, total: int, hight_lvl: int, tresort: int, lvl_up: int):
-        self.total = total
-        self.hight_lvl = hight_lvl
-        self.tresort = tresort
-        self.lvl_up = lvl_up
+def EventAnalyzer(event: str, action: dict):
+    print("la", action[event])
 
 
 def main():
-    player = [{"name": "alice",
-                "lvl": 0
-            },
-            {
-                "name": "ben",
-                "lvl": 0
-            },
-            {
-                "name": "aka",
-                "lvl": 0
-            }
-        ]
-    action = {"lvl up", "open tresor", "kill mob"}
-    lvl = {0, 0, 0}
+    player = [
+        {"name": "alice", "lvl": 0},
+        {"name": "ben", "lvl": 0},
+        {"name": "aka", "lvl": 0},
+    ]
+    action = {"lvl up": 0, "open tresor": 0, "kill mob": 0}
+
     Game = Game_Even_Generator(player, action)
 
     print("=== Game Data Stream Processor ===")
@@ -44,7 +36,12 @@ def main():
     print("Processing 1000 game events...")
     for event in Game.generate_event(3):
         print(event)
-
+    print("game ago ...")
+    for event in Game.generate_event(1000):
+        pass
+    EventAnalyzer("lvl up", action)
+    print(player[2])
+    print(action)
     print("bijour")
 
 
