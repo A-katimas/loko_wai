7fcb9aa8376ba564a8e8804d2cdcaf7d5a551a76
Julie Tardieu <jtardieu@k1r1p10.42mulhouse.fr>
2026-03-11 05:03:52 +0100

    fin du module 4 et 5 debut du 6 et wow c'est cool

---
+++ module06/alchemy/potions.py
@@ -0,0 +1,30 @@
+from alchemy import elements
+
+
+def healing_potion():
+    return (
+        f"Healing potion brewed with {elements.create_fire()} "
+        + f"and {elements.create_water()}"
+    )
+
+
+def strength_potion():
+    return (
+        f"Strength potion brewed with {elements.create_earth()}"
+        + f" and {elements.create_fire()}"
+    )
+
+
+def invisibility_potion():
+    return (
+        f"Invisibility potion brewed with {elements.create_air()}"
+        + f" and {elements.create_water()}"
+    )
+
+
+def wisdom_potion():
+    return (
+        f"Wisdom potion brewed with all elements: {elements.create_air()}"
+        + f" ,{elements.create_water()}, {elements.create_earth()},"
+        + f" {elements.create_fire()}"
+    )
---
+++ module06/ft_sacred_scroll.py
@@ -0,0 +1,38 @@
+import alchemy
+from alchemy.potions import healing_potion as heal
+
+
+def main():
+
+    print("test direct :")
+    print(alchemy.elements.create_fire())
+    print(alchemy.elements.create_air())
+    print(alchemy.elements.create_earth())
+    print(alchemy.elements.create_water())
+
+    print("\ntry not direct:")
+    try:
+        print(alchemy.create_air())
+    except:
+        print("i can't")
+    try:
+        print(alchemy.create_fire())
+    except:
+        print("i can't")
+    try:
+        print(alchemy.create_earth())
+    except:
+        print("i can't")
+    try:
+        print(alchemy.create_water())
+    except:
+        print("i can't")
+
+    print("\n=== import potions ===")
+    print(heal())
+
+    print("\nversion : ", alchemy.__version__)
+    print("author:", alchemy.__author__)
+
+
+main()
